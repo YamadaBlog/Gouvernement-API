dceece9bc6af3683fcc9150713a396b7abf51cac
YamadaBlog <yamadaablog@gmail.com>
2025-05-14 09:43:37 +0200

    Commit

--- Sukuna.Service/Services/UtilisateurService.cs
+++ Sukuna.Service/Services/UtilisateurService.cs
@@ -5,55 +5,54 @@ using Sukuna.Business.Interfaces;
 using Sukuna.Common.Models;
 using Sukuna.DataAccess.Data;
 
-namespace Sukuna.Service
+namespace Sukuna.Service;
+
+public class UtilisateurService : IUtilisateurService
 {
-    public class UtilisateurService : IUtilisateurService
+    private readonly DataContext _context;
+
+    public UtilisateurService(DataContext context)
     {
-        private readonly DataContext _context;
+        _context = context;
+    }
 
-        public UtilisateurService(DataContext context)
-        {
-            _context = context;
-        }
+    public async Task<IEnumerable<Utilisateur>> GetAllUtilisateursAsync()
+    {
+        return await _context.Utilisateurs.ToListAsync();
+    }
 
-        public async Task<IEnumerable<Utilisateur>> GetAllUtilisateursAsync()
-        {
-            return await _context.Utilisateurs.ToListAsync();
-        }
+    public async Task<Utilisateur> GetUtilisateurByIdAsync(int id)
+    {
+        return await _context.Utilisateurs.FindAsync(id);
+    }
 
-        public async Task<Utilisateur> GetUtilisateurByIdAsync(int id)
-        {
-            return await _context.Utilisateurs.FindAsync(id);
-        }
+    public async Task CreateUtilisateurAsync(Utilisateur utilisateur)
+    {
+        await _context.Utilisateurs.AddAsync(utilisateur);
+    }
 
-        public async Task CreateUtilisateurAsync(Utilisateur utilisateur)
-        {
-            await _context.Utilisateurs.AddAsync(utilisateur);
-        }
+    public async Task<Utilisateur> GetAuthauthUser(string userEmail, string userMpd)
+    {
+        return await _context.Utilisateurs
+            .FirstOrDefaultAsync(c => c.Email == userEmail && c.MotDePasse == userMpd);
+    }
 
-        public async Task<Utilisateur> GetAuthauthUser(string userEmail, string userMpd)
-        {
-            return await _context.Utilisateurs
-                .FirstOrDefaultAsync(c => c.Email == userEmail && c.MotDePasse == userMpd);
-        }
+    public async Task UpdateUtilisateurAsync(Utilisateur utilisateur)
+    {
+        _context.Utilisateurs.Update(utilisateur);
+    }
 
-        public async Task UpdateUtilisateurAsync(Utilisateur utilisateur)
+    public async Task DeleteUtilisateurAsync(int id)
+    {
+        var utilisateur = await GetUtilisateurByIdAsync(id);
+        if (utilisateur != null)
         {
-            _context.Utilisateurs.Update(utilisateur);
+            _context.Utilisateurs.Remove(utilisateur);
         }
+    }
 
-        public async Task DeleteUtilisateurAsync(int id)
-        {
-            var utilisateur = await GetUtilisateurByIdAsync(id);
-            if (utilisateur != null)
-            {
-                _context.Utilisateurs.Remove(utilisateur);
-            }
-        }
-
-        public async Task<bool> SaveAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
+    public async Task<bool> SaveAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
     }
 }
